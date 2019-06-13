50c711eae2a92b697e4037597bdf1b8559a9f6b6
Andrew Yueh <yueh.andrew@gmail.com>
2019-06-13 10:26:37 -0700

    update

--- src/App.js
+++ src/App.js
@@ -9,6 +9,16 @@ import Scrolling from './components/Scrolling';
 import FixedPositionInContainer from './components/FixedPositionInContainer';
 import SectionSelector from './components/SectionSelector';
 
+const sectionTitleMap = {
+  containers: 'Containers',
+  position: 'Position',
+  flexBox: 'flexbox',
+  parallelAlignment: 'Parallel Alignment',
+  percentDimensions: 'Percent Dimensions',
+  scrolling: 'Scrolling',
+  fixedRelativePosition: 'Fixed Relative Positioning',
+}
+
 class App extends Component {
   constructor() {
     super()
@@ -16,16 +26,27 @@ class App extends Component {
       containers: false,
       position: false,
       flexBox: false,
-      parallelAlignment: false,
-      percentDimensions: false,
       scrolling: true,
+      percentDimensions: false,
       fixedRelativePosition: false,
+      parallelAlignment: false,
     }
   }
 
   handleShow = (type) => () => {
     Object.keys(this.state).forEach(component => this.setState({ [component]: false }))
     this.setState({ [type]: !this.state[type] })
+  }
+
+  sections = () => {
+    return Object.keys(this.state).map(section => (
+      <SectionSelector
+        focused={this.state[section]}
+        onClick={this.handleShow(section)}
+      >
+        {sectionTitleMap[section]}
+      </SectionSelector>
+    ))
   }
 
   render() {
@@ -40,67 +61,36 @@ class App extends Component {
     } = this.state
     return (
       <div className="App" style={{ marginTop: 50 }}>
-        <div style={{ background: 'white', zIndex: 100, position: 'fixed', width: '100vw', display: 'flex', alignItems: 'center', flexDirection: 'column', top: 0, borderBottom: '1px solid black' }}>
+        <div style={{
+          background: 'white',
+          zIndex: 100,
+          position: 'fixed',
+          width: '100vw',
+          display: 'flex',
+          alignItems: 'center',
+          flexDirection: 'column',
+          top: 0,
+          borderBottom: '1px solid black'
+        }}>
           <h2 style={{ display: 'flex', justifyContent: 'center' }}>
             CSS demos
           </h2>
           <div>
             <div style={{ marginBottom: 30, display: 'flex', justifyContent: 'center' }}>
-              <SectionSelector
-                focused={position}
-                onClick={this.handleShow('position')}
-              >
-                Position
-              </SectionSelector>
-              <SectionSelector
-                focused={containers}
-                onClick={this.handleShow('containers')}
-              >
-                Containers
-              </SectionSelector>
-              <SectionSelector
-                focused={flexBox}
-                onClick={this.handleShow('flexBox')}
-              >
-                Flexbox
-              </SectionSelector>
-              <SectionSelector
-                focused={percentDimensions}
-                onClick={this.handleShow('percentDimensions')}
-              >
-                Percentage dimensions
-              </SectionSelector>
-              <SectionSelector
-                focused={scrolling}
-                onClick={this.handleShow('scrolling')}
-              >
-                Scrolling
-              </SectionSelector>
-              <SectionSelector
-                focused={fixedRelativePosition}
-                onClick={this.handleShow('fixedRelativePosition')}
-              >
-                Fixed relative positioning
-              </SectionSelector>
-              <SectionSelector
-                focused={parallelAlignment}
-                onClick={this.handleShow('parallelAlignment')}
-              >
-                Parallel alignment
-              </SectionSelector>
+              {this.sections()}
             </div>
           </div>
-          </div>
-          <div style={{marginTop: 150}}>
-            {position ? <Position /> : null}
-            {containers ? <Containers /> : null}
-            {flexBox ? <Flexbox /> : null}
-            {parallelAlignment ? <ParallelChildrenAlignment /> : null}
-            {percentDimensions ? <PercentDimensions /> : null}
-            {scrolling ? <Scrolling /> : null}
-            {fixedRelativePosition ? <FixedPositionInContainer /> : null}
-          </div>
         </div>
+        <div style={{ marginTop: 150 }}>
+          {position ? <Position /> : null}
+          {containers ? <Containers /> : null}
+          {flexBox ? <Flexbox /> : null}
+          {parallelAlignment ? <ParallelChildrenAlignment /> : null}
+          {percentDimensions ? <PercentDimensions /> : null}
+          {scrolling ? <Scrolling /> : null}
+          {fixedRelativePosition ? <FixedPositionInContainer /> : null}
+        </div>
+      </div>
     );
   }
 }
